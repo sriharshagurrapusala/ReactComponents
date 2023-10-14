b049b5531e12dff11090dbee6a1af6999e66f242
SRI HARSHA GURRAPUSALA <112601670+sriharshagurrapusala@users.noreply.github.com>
2023-10-14 14:37:58 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+    </ul>
+  );
+}
+
+export default List;
